ceb7d0a2a4510c1ce4f4b14f9628dcbb97c7f64c
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-13 12:52:06 +0200

    aggiunto controllo post trips

--- src/main/java/mirkoabozzi/U5S6L5/repositories/TripsRepository.java
+++ src/main/java/mirkoabozzi/U5S6L5/repositories/TripsRepository.java
@@ -4,8 +4,11 @@ import mirkoabozzi.U5S6L5.entities.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Repository
 public interface TripsRepository extends JpaRepository<Trip, UUID> {
+
+    boolean existsByDestinationAndDate(String destination, LocalDate date);
 }
--- src/main/java/mirkoabozzi/U5S6L5/services/TripsService.java
+++ src/main/java/mirkoabozzi/U5S6L5/services/TripsService.java
@@ -2,6 +2,7 @@ package mirkoabozzi.U5S6L5.services;
 
 import mirkoabozzi.U5S6L5.dto.TripsDTO;
 import mirkoabozzi.U5S6L5.entities.Trip;
+import mirkoabozzi.U5S6L5.exceptions.BadRequestException;
 import mirkoabozzi.U5S6L5.repositories.TripsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,8 @@ public class TripsService {
 
     //POST
     public Trip save(TripsDTO payload) {
+        if (tripsRepository.existsByDestinationAndDate(payload.destination(), payload.date()))
+            throw new BadRequestException("This trip " + payload.destination() + " on this date " + payload.date() + " already exists on DB");
         Trip trip = new Trip(payload.destination(), payload.date(), payload.state());
         return this.tripsRepository.save(trip);
     }
